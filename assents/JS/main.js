30731f722548bdd35f2ea9ed7bf0177b5acc74b1
guilhermefrigotto <guilherme.frigotto@gmail.com>
2023-01-04 17:02:26 -0400

    portfolio

--- assents/JS/main.js
+++ assents/JS/main.js
@@ -40,3 +40,8 @@ sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
 
+// ALERT
+
+function sent(){
+    alert ("Message sent")
+}
